9d3eddf2839d171234d3d9fada12fa95448e68ed
samuel1245678 <samuel89g3@hotmail.com>
2024-07-31 15:23:26 -0300

    Main view Controller

--- src/module-info.java
+++ src/module-info.java
@@ -3,4 +3,7 @@ module workshopjavafxjdbc2 {
 	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+    requires java.sql;
+    opens gui to javafx.fxml; // Adicione esta linha para exportar o pacote gui para javafx.fxm
+    exports gui; // Se você também precisar exportar o pacote gui para outros módulos
 }
