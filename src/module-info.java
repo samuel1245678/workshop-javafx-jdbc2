821a92c3e9ca5ab0cb01a11c1763d883faa1add5
samuel1245678 <samuel89g3@hotmail.com>
2024-08-02 20:40:26 -0300

    Departmentservice

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,19 @@
 module workshopjavafxjdbc2 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
-    requires java.sql;
     opens gui to javafx.fxml; // Adicione esta linha para exportar o pacote gui para javafx.fxm
     exports gui; // Se você também precisar exportar o pacote gui para outros módulos
     
+    
+    opens model.entities to javafx.base; // Adicione esta linha para abrir o pacote model.entities para javafx.base
+
+    exports model.entities; // Se você também precisar exportar o pacote model.entities para outros módulos
    
+
+
+
+    
 }
