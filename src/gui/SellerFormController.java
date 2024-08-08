44b605aeca82856dc83df0bc4095afa49f22675c
samuel1245678 <samuel89g3@hotmail.com>
2024-08-08 18:17:52 -0300

    Saving seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -125,6 +127,8 @@ public class SellerFormController implements Initializable {
 
 	}
 
+	// ELE PEGA OS DADOS QUE FORAM PREENCHIDO LA NO FORMULARIO AI ELE CARREGA UM
+	// OBJETO COM ESSES DADOS
 	private Seller getFormData() {
 		Seller obj = new Seller();
 
@@ -137,6 +141,30 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field cant`t be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("email", "Field cant`t be empty");
+
+		} else {
+			// ESSA VARIAVEL INSTANT VAI RECEBER O CONTEUDO LA DO DatePicker
+			// ATSTARTOFDAY(ZONE) ELE CONVERTE A DATA ESCOLHIDA NO HORARIO DO COMPUTADOR DO
+			// USUARIO PARA INSTANT
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		// PARA TESTAR O SALARIO SE NULO OU VAZIO
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can`t be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -160,7 +188,7 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
-		
+
 		initializeComboBoxDepartment();
 	}
 
@@ -176,11 +204,10 @@ public class SellerFormController implements Initializable {
 		if (entity.getBirthDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
-		
-		if(entity.getDepartment() == null) {
+
+		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
-		}
-		else {
+		} else {
 			comboBoxDepartment.setValue(entity.getDepartment());
 		}
 	}
@@ -196,12 +223,15 @@ public class SellerFormController implements Initializable {
 		comboBoxDepartment.setItems(obsList);
 	}
 
+	// ESSE METODO setErrorMessages VAI TESTAR CADA UM DOS POSSIVEIS
+	// ERROS AI ELE SETAR O VALOR DO LEBAL CORRESPONDENTE
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
-
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 	}
 
 	private void initializeComboBoxDepartment() {
